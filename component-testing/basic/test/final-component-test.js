9e24831caede5133df3502a001815a36a0e2e82b
i0natan <me@goldbergyoni.com>
2019-10-17 18:13:38 +0300

    Genric message

--- component-testing/basic/test/final-component-test.js
+++ component-testing/basic/test/final-component-test.js
@@ -49,7 +49,7 @@ afterEach(() => {
 });
 
 /*eslint-disable */
-describe('/api #final', () => {
+describe.skip('/api #final', () => {
   describe("POST /orders", () => {
     test("When adding a new valid order , Then should get back 200 response", async () => {
       //Arrange
