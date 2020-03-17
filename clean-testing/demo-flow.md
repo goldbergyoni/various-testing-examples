be8405db23372f2cb3e835a89a4ee2b82cbb6d9b
Yoni <me@goldbergyoni.com>
2020-03-17 21:29:35 +0200

    Genric message

--- clean-testing/demo-flow.md
+++ clean-testing/demo-flow.md
@@ -11,27 +11,28 @@ How it often looks, how it should look like, micro-contribution, the defining mo
 
 ## Test structure patterns
 
+- Show title "The test structure"
+
 ### Naming
 
 - Problem: A test fails, I'm about to deploy, what is it about? now have to read the code 📹
 - Guideline: Write for the visitor, speak at the requirement level, show requirement document 🖼
-- Requirements structure: topic, when, then...
 - Advice: include 3 parts, from Osherove book 📓
 - BP: 3 parts in each test name ✅
 - First try: code 3 parts in one line, report looks bad 🖼
 - Linter: eslint-plugin-test-names ️️⚙️
-a
 
 ### Hirearchy
 
 - Problem: Long textual list, any relation between the failing test?
+- Making this better: organized report 🖼, can run only one category 🆒
 - Applying categorization: Group by common
-- Method 1: Group by scenario, great when there are few scenarios and many tests, like
+- Method 1: Group by scenario, great when there are few scenarios and many tests, like, the source of "it", Mocha context
 - Method 2: Group by custom category, create describe, put tests
-- The result: organized report 🖼, can run only one category 🆒
-- BP: Categorize your tests ✅
 - Tagging: sometime we want to run custom
+- BP: Categorize your tests ✅
 - Linter: ⚙️
+- Here's a list with useful linters 🎁
 
 ### The test structure
 
--- clean-testing/src/test/transfer-service-test-edit.js
+++ clean-testing/src/test/transfer-service-test-edit.js
@@ -1,24 +1,151 @@
+// inheritance, global object, abstract helper, try-catch, magic number, similar cases,
+// foo input, name & hirearchy, test reports,
 const TransferService = require('../transfer-service');
+const testHelpers = require('./test-helpers');
+const bankingProvider = require('../banking-provider');
+const dbRepository = require('../db-repository');
 
+
+let serviceUnderTest;
+const fromWhomUser = {
+  name: 'Kent Beck',
+  countryCode: 'IL',
+  allowedCredit: 100,
+};
 describe('Transfer Service', () => {
-  // Anti-pattern test
-  test('When transfer exceeds user credit, then transfer shouldnt appear in user transfers', () => {
-    // Arrange
-    const serviceUnderTest = new TransferService();
-    const aTransferWithoutCredit = helper.getTransferJSON({
-      user: {
-        credit: 50,
-      },
-      howMuch: 100,
-    });
-    const transferResponse = serviceUnderTest.transfer(aTransferWithoutCredit);
-
-    // Act
-    const allUserTransfers = serviceUnderTest.getTransfers(aTransferWithoutCredit.user.name);
-
-    // Assert
-    expect(allUserTransfers).not.toContainEqual(aTransferWithoutCredit);
+  beforeAll(async () => { // ❌
+    const options = {
+      creditPolicy: 'zero',
+    };
+    serviceUnderTest = new TransferService(options, dbRepository, bankingProvider);
   });
+
+  // ❌
+  test('Transfer service -> When not enough credit, then transfer should not be saved', () => {
+    const unauthorizedTransferToAdd = testHelpers.factorMoneyTransfer({});
+    const transferResponse = serviceUnderTest.transfer(fromWhomUser, {}, 110, 'Bank of America');
+    const allUserTransfers = serviceUnderTest.getTransfers(unauthorizedTransferToAdd.user.name);
+    expect(transferResponse.status).toBe('declined');
+    expect(serviceUnderTest.lastOneApproved).toBe(false);
+
+    if (allUserTransfers === true) {
+      let transferFound2 = false;
+      transferFound2 = true;
+      console.log(transferFound2);
+    }
+
+    // check that transfer was not saved
+    let transferFound = false;
+    allUserTransfers.forEach((singleTransfer) => {
+      if (singleTransfer === unauthorizedTransferToAdd) {
+        transferFound = true;
+      }
+    });
+    expect(transferFound).toBe(false);
+  });
+
+  test('Should throw exception', () => {
+    const transferServiceUnderTest = new TransferService();
+    let wasErrorFound = false;
+    let errorType = 'invalidInput';
+
+    try { // ❌
+      transferServiceUnderTest.transfer(null, null, null, null); // Didn't provide mandatory inputs
+    } catch (e) {
+      wasErrorFound = true;
+      errorType = e.code;
+    }
+
+    expect(wasErrorFound).toBe(true);
+    expect(errorType).toBe('invalidInput');
+  });
+
+  test('When user is deleted, should not approve the transfer', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true, 'it should be like this').toBe(true);
+  });
+
+  test('When user has no credit, should not approve the transfer', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When receiver lives in forbidden country, should not approve the transfer', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When no sender specified, should throw invalid details error', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(false);
+  });
+
+  test('When no receiver specified, should throw invalid details error', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(false);
+  });
+
+
+  test('When no bank specified, should throw invalid details error', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When user did not confirm his account, should not approve the transfer', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When user has enough credit, should approve the transfer', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When transfer is successful, should send mail to receiver', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When transfer is successful, receiver balance should get topped up', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When no amount specified, should throw invalid details error', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(false);
+  });
+
+  test('When transfer is with different currency, receiver gets using his own currency', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When sender from Italy sends a valid payment, transfer is approved', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When sender from India sends a valid payment, transfer is approved', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When sender from US sends a valid payment, transfer is approved', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When sender from Germany sends a valid payment, transfer is approved', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
+  test('When sender from Argentina sends a valid payment, transfer is approved', () => {
+    // This test is here only to exemplify how big test reports look like
+    expect(true).toBe(true);
+  });
+
 
   test.skip('When trying to exceed credit, transfer doesnt appear in user history', () => {
     // Arrange
@@ -35,26 +162,6 @@ describe('Transfer Service', () => {
     const allUserTransfers = serviceUnderTest.getTransfers(unauthorizedTransferToAdd.user.name);
 
     // Assert
-    expect(allUserTransfers).not.toContain(unauthorizedTransferToAdd);
+    expect(allUserTransfers).not.toContainEqual(unauthorizedTransferToAdd);
   });
 });
-
-const helper = {
-  getTransferJSON: (transferProperties) => {
-    const defaultTransfer = {
-      user: {
-        name: 'Daniel',
-        credit: 30,
-      },
-      howMuch: 100,
-      toWhom: 'Rose',
-      bank: 'Bank Of America',
-    };
-
-    return Object.assign(transferProperties, defaultTransfer);
-  },
-  configureTransferService: () => {
-    // I don't really do anything
-  },
-}
-;
